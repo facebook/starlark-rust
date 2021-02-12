60cd5820fcfb318b07eb0ac495ae48a738b64d0e
Neil Mitchell <ndmitchell@fb.com>
2021-02-12 03:41:26 -0800

    In the lexer tests, don't keep repeating Token::
    Summary: Half of the test results are just Token:: after each other - remove some.
    
    Reviewed By: bobyangyf
    
    Differential Revision: D26373738
    
    fbshipit-source-id: 311f22f29ab3c5c07134d5adcd88305edb46a235

--- starlark/src/syntax/lexer_tests.rs
+++ starlark/src/syntax/lexer_tests.rs
@@ -17,7 +17,7 @@
 
 use crate::syntax::{
     dialect::Dialect,
-    lexer::{Lexer, LexerError, Token},
+    lexer::{Lexer, LexerError, Token, Token::*},
     testing::{assert_diagnostics, testcase_files},
 };
 use codemap::CodeMap;
@@ -51,8 +51,8 @@ fn test_int_lit() {
         collect_result(s)
             .iter()
             .filter_map(|v| match v {
-                Token::IntegerLiteral(r) => Some(*r),
-                Token::Newline => None,
+                IntegerLiteral(r) => Some(*r),
+                Newline => None,
                 _ => panic!("{:?} is not a integer literal", v),
             })
             .collect()
@@ -79,32 +79,9 @@ fn test_indentation() {
     );
     assert_eq!(
         &[
-            Token::Newline,
-            Token::Plus,
-            Token::Newline,
-            Token::Indent,
-            Token::Minus,
-            Token::Newline,
-            Token::Indent,
-            Token::Slash,
-            Token::Newline,
-            Token::Star,
-            Token::Newline,
-            Token::Dedent,
-            Token::Equal,
-            Token::Newline,
-            Token::Indent,
-            Token::Percent,
-            Token::Newline,
-            Token::Indent,
-            Token::Dot,
-            Token::Newline,
-            Token::Dedent,
-            Token::Dedent,
-            Token::Dedent,
-            Token::PlusEqual,
-            Token::Newline,
-            Token::Newline,
+            Newline, Plus, Newline, Indent, Minus, Newline, Indent, Slash, Newline, Star, Newline,
+            Dedent, Equal, Newline, Indent, Percent, Newline, Indent, Dot, Newline, Dedent, Dedent,
+            Dedent, PlusEqual, Newline, Newline,
         ],
         &r[..]
     );
@@ -117,38 +94,38 @@ fn test_symbols() {
     );
     assert_eq!(
         &[
-            Token::Comma,
-            Token::Semicolon,
-            Token::Colon,
-            Token::PlusEqual,
-            Token::MinusEqual,
-            Token::StarEqual,
-            Token::SlashEqual,
-            Token::DoubleSlashEqual,
-            Token::PercentEqual,
-            Token::DoubleEqual,
-            Token::BangEqual,
-            Token::LessEqual,
-            Token::GreaterEqual,
-            Token::DoubleStar,
-            Token::Equal,
-            Token::LessThan,
-            Token::GreaterThan,
-            Token::Minus,
-            Token::Plus,
-            Token::Star,
-            Token::Percent,
-            Token::Slash,
-            Token::DoubleSlash,
-            Token::Dot,
-            Token::OpeningCurly,
-            Token::ClosingCurly,
-            Token::OpeningSquare,
-            Token::ClosingSquare,
-            Token::OpeningRound,
-            Token::ClosingRound,
-            Token::Pipe,
-            Token::Newline,
+            Comma,
+            Semicolon,
+            Colon,
+            PlusEqual,
+            MinusEqual,
+            StarEqual,
+            SlashEqual,
+            DoubleSlashEqual,
+            PercentEqual,
+            DoubleEqual,
+            BangEqual,
+            LessEqual,
+            GreaterEqual,
+            DoubleStar,
+            Equal,
+            LessThan,
+            GreaterThan,
+            Minus,
+            Plus,
+            Star,
+            Percent,
+            Slash,
+            DoubleSlash,
+            Dot,
+            OpeningCurly,
+            ClosingCurly,
+            OpeningSquare,
+            ClosingSquare,
+            OpeningRound,
+            ClosingRound,
+            Pipe,
+            Newline,
         ],
         &r[..]
     );
@@ -161,24 +138,8 @@ fn test_keywords() {
     );
     assert_eq!(
         &[
-            Token::And,
-            Token::Else,
-            Token::Load,
-            Token::Break,
-            Token::For,
-            Token::Not,
-            Token::Not,
-            Token::In,
-            Token::Continue,
-            Token::If,
-            Token::Or,
-            Token::Def,
-            Token::In,
-            Token::Pass,
-            Token::Elif,
-            Token::Return,
-            Token::Lambda,
-            Token::Newline,
+            And, Else, Load, Break, For, Not, Not, In, Continue, If, Or, Def, In, Pass, Elif,
+            Return, Lambda, Newline,
         ],
         &r[..]
     );
@@ -191,28 +152,28 @@ fn test_number_collated_with_keywords_or_identifier() {
         collect_result("0in 1and 2else 3load 4break 5for 6not 7not  in 8continue 10identifier11");
     assert_eq!(
         &[
-            Token::IntegerLiteral(0),
-            Token::In,
-            Token::IntegerLiteral(1),
-            Token::And,
-            Token::IntegerLiteral(2),
-            Token::Else,
-            Token::IntegerLiteral(3),
-            Token::Load,
-            Token::IntegerLiteral(4),
-            Token::Break,
-            Token::IntegerLiteral(5),
-            Token::For,
-            Token::IntegerLiteral(6),
-            Token::Not,
-            Token::IntegerLiteral(7),
-            Token::Not,
-            Token::In,
-            Token::IntegerLiteral(8),
-            Token::Continue,
-            Token::IntegerLiteral(10),
-            Token::Identifier("identifier11".to_owned()),
-            Token::Newline,
+            IntegerLiteral(0),
+            In,
+            IntegerLiteral(1),
+            And,
+            IntegerLiteral(2),
+            Else,
+            IntegerLiteral(3),
+            Load,
+            IntegerLiteral(4),
+            Break,
+            IntegerLiteral(5),
+            For,
+            IntegerLiteral(6),
+            Not,
+            IntegerLiteral(7),
+            Not,
+            In,
+            IntegerLiteral(8),
+            Continue,
+            IntegerLiteral(10),
+            Identifier("identifier11".to_owned()),
+            Newline,
         ],
         &r[..]
     );
@@ -226,22 +187,22 @@ fn test_reserved() {
     );
     assert_eq!(
         &[
-            Token::Reserved("as".to_owned()),
-            Token::Reserved("import".to_owned()),
-            Token::Reserved("is".to_owned()),
-            Token::Reserved("class".to_owned()),
-            Token::Reserved("nonlocal".to_owned()),
-            Token::Reserved("del".to_owned()),
-            Token::Reserved("raise".to_owned()),
-            Token::Reserved("except".to_owned()),
-            Token::Reserved("try".to_owned()),
-            Token::Reserved("finally".to_owned()),
-            Token::Reserved("while".to_owned()),
-            Token::Reserved("from".to_owned()),
-            Token::Reserved("with".to_owned()),
-            Token::Reserved("global".to_owned()),
-            Token::Reserved("yield".to_owned()),
-            Token::Newline,
+            Reserved("as".to_owned()),
+            Reserved("import".to_owned()),
+            Reserved("is".to_owned()),
+            Reserved("class".to_owned()),
+            Reserved("nonlocal".to_owned()),
+            Reserved("del".to_owned()),
+            Reserved("raise".to_owned()),
+            Reserved("except".to_owned()),
+            Reserved("try".to_owned()),
+            Reserved("finally".to_owned()),
+            Reserved("while".to_owned()),
+            Reserved("from".to_owned()),
+            Reserved("with".to_owned()),
+            Reserved("global".to_owned()),
+            Reserved("yield".to_owned()),
+            Newline,
         ],
         &r[..]
     );
@@ -250,23 +211,13 @@ fn test_reserved() {
 #[test]
 fn test_comment() {
     // Comment should be ignored
-    assert_eq!(collect_result("# a comment\n"), &[Token::Newline]);
-    assert_eq!(collect_result(" # a comment\n"), &[Token::Newline]);
+    assert_eq!(collect_result("# a comment\n"), &[Newline]);
+    assert_eq!(collect_result(" # a comment\n"), &[Newline]);
     let r = collect_result("a # a comment\n");
-    assert_eq!(
-        &[
-            Token::Identifier("a".to_owned()),
-            Token::Newline,
-            Token::Newline
-        ],
-        &r[..]
-    );
+    assert_eq!(&[Identifier("a".to_owned()), Newline, Newline], &r[..]);
     // But it should not eat everything
     let r = collect_result("[\n# a comment\n]");
-    assert_eq!(
-        &[Token::OpeningSquare, Token::ClosingSquare, Token::Newline,],
-        &r[..]
-    );
+    assert_eq!(&[OpeningSquare, ClosingSquare, Newline,], &r[..]);
 }
 
 #[test]
@@ -274,12 +225,12 @@ fn test_identifier() {
     let r = collect_result("a identifier CAPS _CAPS _0123");
     assert_eq!(
         &[
-            Token::Identifier("a".to_owned()),
-            Token::Identifier("identifier".to_owned()),
-            Token::Identifier("CAPS".to_owned()),
-            Token::Identifier("_CAPS".to_owned()),
-            Token::Identifier("_0123".to_owned()),
-            Token::Newline,
+            Identifier("a".to_owned()),
+            Identifier("identifier".to_owned()),
+            Identifier("CAPS".to_owned()),
+            Identifier("_CAPS".to_owned()),
+            Identifier("_0123".to_owned()),
+            Newline,
         ],
         &r[..]
     );
@@ -290,17 +241,17 @@ fn test_string_lit() {
     let r = collect_result("'123' \"123\" '' \"\" '\\'' \"\\\"\" '\"' \"'\" '\\n' '\\w'");
     assert_eq!(
         &[
-            Token::StringLiteral("123".to_owned()),
-            Token::StringLiteral("123".to_owned()),
-            Token::StringLiteral("".to_owned()),
-            Token::StringLiteral("".to_owned()),
-            Token::StringLiteral("'".to_owned()),
-            Token::StringLiteral("\"".to_owned()),
-            Token::StringLiteral("\"".to_owned()),
-            Token::StringLiteral("'".to_owned()),
-            Token::StringLiteral("\n".to_owned()),
-            Token::StringLiteral("\\w".to_owned()),
-            Token::Newline,
+            StringLiteral("123".to_owned()),
+            StringLiteral("123".to_owned()),
+            StringLiteral("".to_owned()),
+            StringLiteral("".to_owned()),
+            StringLiteral("'".to_owned()),
+            StringLiteral("\"".to_owned()),
+            StringLiteral("\"".to_owned()),
+            StringLiteral("'".to_owned()),
+            StringLiteral("\n".to_owned()),
+            StringLiteral("\\w".to_owned()),
+            Newline,
         ],
         &r[..]
     );
@@ -318,13 +269,13 @@ fn test_string_lit() {
     let r = collect_result("'''''' '''\\n''' '''\n''' \"\"\"\"\"\" \"\"\"\\n\"\"\" \"\"\"\n\"\"\"");
     assert_eq!(
         &[
-            Token::StringLiteral("".to_owned()),
-            Token::StringLiteral("\n".to_owned()),
-            Token::StringLiteral("\n".to_owned()),
-            Token::StringLiteral("".to_owned()),
-            Token::StringLiteral("\n".to_owned()),
-            Token::StringLiteral("\n".to_owned()),
-            Token::Newline,
+            StringLiteral("".to_owned()),
+            StringLiteral("\n".to_owned()),
+            StringLiteral("\n".to_owned()),
+            StringLiteral("".to_owned()),
+            StringLiteral("\n".to_owned()),
+            StringLiteral("\n".to_owned()),
+            Newline,
         ],
         &r[..]
     );
@@ -332,14 +283,14 @@ fn test_string_lit() {
     let r = collect_result("r'' r\"\" r'\\'' r\"\\\"\" r'\"' r\"'\" r'\\n'");
     assert_eq!(
         &[
-            Token::StringLiteral("".to_owned()),
-            Token::StringLiteral("".to_owned()),
-            Token::StringLiteral("'".to_owned()),
-            Token::StringLiteral("\"".to_owned()),
-            Token::StringLiteral("\"".to_owned()),
-            Token::StringLiteral("'".to_owned()),
-            Token::StringLiteral("\\n".to_owned()),
-            Token::Newline,
+            StringLiteral("".to_owned()),
+            StringLiteral("".to_owned()),
+            StringLiteral("'".to_owned()),
+            StringLiteral("\"".to_owned()),
+            StringLiteral("\"".to_owned()),
+            StringLiteral("'".to_owned()),
+            StringLiteral("\\n".to_owned()),
+            Newline,
         ],
         &r[..]
     );
@@ -357,24 +308,24 @@ def _impl(ctx):
     );
     assert_eq!(
         &[
-            Token::StringLiteral("A docstring.".to_owned()),
-            Token::Newline,
-            Token::Newline,
-            Token::Def,
-            Token::Identifier("_impl".to_owned()),
-            Token::OpeningRound,
-            Token::Identifier("ctx".to_owned()),
-            Token::ClosingRound,
-            Token::Colon,
-            Token::Newline,
-            Token::Indent,
-            Token::Identifier("print".to_owned()),
-            Token::OpeningRound,
-            Token::StringLiteral("Hello, World!".to_owned()),
-            Token::ClosingRound,
-            Token::Newline,
-            Token::Dedent,
-            Token::Newline,
+            StringLiteral("A docstring.".to_owned()),
+            Newline,
+            Newline,
+            Def,
+            Identifier("_impl".to_owned()),
+            OpeningRound,
+            Identifier("ctx".to_owned()),
+            ClosingRound,
+            Colon,
+            Newline,
+            Indent,
+            Identifier("print".to_owned()),
+            OpeningRound,
+            StringLiteral("Hello, World!".to_owned()),
+            ClosingRound,
+            Newline,
+            Dedent,
+            Newline,
         ],
         &r[..]
     );
@@ -385,9 +336,9 @@ fn test_escape_newline() {
     let r = collect_result("a \\\nb");
     assert_eq!(
         &[
-            Token::Identifier("a".to_owned()),
-            Token::Identifier("b".to_owned()),
-            Token::Newline,
+            Identifier("a".to_owned()),
+            Identifier("b".to_owned()),
+            Newline,
         ],
         &r[..]
     );
@@ -404,11 +355,11 @@ cmd = """A \
     );
     assert_eq!(
         &[
-            Token::Newline,
-            Token::Identifier("cmd".to_owned()),
-            Token::Equal,
-            Token::StringLiteral("A     B     C     ".to_owned()),
-            Token::Newline,
+            Newline,
+            Identifier("cmd".to_owned()),
+            Equal,
+            StringLiteral("A     B     C     ".to_owned()),
+            Newline,
         ],
         &r[..]
     );
@@ -417,28 +368,28 @@ cmd = """A \
 #[test]
 fn test_span() {
     let expected = vec![
-        (0, Token::Newline, 1),
-        (1, Token::Def, 4),
-        (5, Token::Identifier("test".to_owned()), 9),
-        (9, Token::OpeningRound, 10),
-        (10, Token::Identifier("a".to_owned()), 11),
-        (11, Token::ClosingRound, 12),
-        (12, Token::Colon, 13),
-        (13, Token::Newline, 14),
-        (14, Token::Indent, 16),
-        (16, Token::Identifier("fail".to_owned()), 20),
-        (20, Token::OpeningRound, 21),
-        (21, Token::Identifier("a".to_owned()), 22),
-        (22, Token::ClosingRound, 23),
-        (23, Token::Newline, 24),
-        (24, Token::Newline, 25),
-        (25, Token::Dedent, 25),
-        (25, Token::Identifier("test".to_owned()), 29),
-        (29, Token::OpeningRound, 30),
-        (30, Token::StringLiteral("abc".to_owned()), 35),
-        (35, Token::ClosingRound, 36),
-        (36, Token::Newline, 37),
-        (37, Token::Newline, 37),
+        (0, Newline, 1),
+        (1, Def, 4),
+        (5, Identifier("test".to_owned()), 9),
+        (9, OpeningRound, 10),
+        (10, Identifier("a".to_owned()), 11),
+        (11, ClosingRound, 12),
+        (12, Colon, 13),
+        (13, Newline, 14),
+        (14, Indent, 16),
+        (16, Identifier("fail".to_owned()), 20),
+        (20, OpeningRound, 21),
+        (21, Identifier("a".to_owned()), 22),
+        (22, ClosingRound, 23),
+        (23, Newline, 24),
+        (24, Newline, 25),
+        (25, Dedent, 25),
+        (25, Identifier("test".to_owned()), 29),
+        (29, OpeningRound, 30),
+        (30, StringLiteral("abc".to_owned()), 35),
+        (35, ClosingRound, 36),
+        (36, Newline, 37),
+        (37, Newline, 37),
     ];
     let actual: Vec<(u64, Token, u64)> = Lexer::new(
         r#"
@@ -462,12 +413,7 @@ x
 # test"#,
     );
     assert_eq!(
-        &[
-            Token::Newline,
-            Token::Identifier("x".to_owned()),
-            Token::Newline,
-            Token::Newline
-        ],
+        &[Newline, Identifier("x".to_owned()), Newline, Newline],
         &r[..]
     );
 }
