a714dddc238b3124626f64adcdfa0230db332e10
Neil Mitchell <ndmitchell@fb.com>
2021-04-30 01:37:11 -0700

    Allow unused_extern_crates in generated code
    Summary: This is a warning that lalrpop generates, so allow the warning to be turned on.
    
    Reviewed By: bobyangyf
    
    Differential Revision: D28091426
    
    fbshipit-source-id: b6ece6f5c9cd38ce3540aeb98b37fc27cbcceab5

--- starlark/src/syntax/mod.rs
+++ starlark/src/syntax/mod.rs
@@ -38,6 +38,7 @@ pub(crate) mod validate;
 #[allow(clippy::inefficient_to_string)]
 #[allow(clippy::trivially_copy_pass_by_ref)]
 #[allow(clippy::too_many_arguments)]
+#[allow(unused_extern_crates)]
 #[cfg_attr(feature = "custom_linter", allow(gazebo_use_dupe))]
 mod grammar {
     include!(concat!(env!("OUT_DIR"), "/syntax/grammar.rs"));
