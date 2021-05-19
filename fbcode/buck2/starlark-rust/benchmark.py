3418b318e188df2c3ac7e349c4ac3ce64c39b7ec
Neil Mitchell <ndmitchell@fb.com>
2021-05-19 03:21:50 -0700

    Add a microbenchmark script
    Summary: Allow comparison with Python on a range of interesting examples.
    
    Reviewed By: bobyangyf
    
    Differential Revision: D28507260
    
    fbshipit-source-id: ed04b2ae0fd2ddf3d0c86b33d08d2affedec2b56

---
+++ fbcode/buck2/starlark-rust/benchmark.py
@@ -0,0 +1,27 @@
+# Python: python benchmark.py
+# Rust: starlark benchmark.py
+
+# Python = 4.35s, Rust = 0.72s
+def benchmark1():
+    for _x in range(100000000):
+        pass
+
+
+# Python = 6.08s, Rust = 2.35s
+def benchmark2():
+    y = 3
+    for _x in range(100000000):
+        y = y * 1
+    return y
+
+
+# Python = 7.04s, Rust = 31.9s
+def benchmark3():
+    y = 0
+    xs = []
+    for _x in range(100000000):
+        y = len(xs)
+    return y
+
+
+print(benchmark3())
