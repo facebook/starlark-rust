b328ab9f0e124d9a2674355bd3df60fc87f3d58d
Neil Mitchell <ndmitchell@fb.com>
2021-05-20 02:06:55 -0700

    Rename _context to _eval
    Summary: Consistency
    
    Reviewed By: blackm00n
    
    Differential Revision: D28539164
    
    fbshipit-source-id: 345bbeb781acba53787eb5098884b4ef23098898

--- starlark/src/values/types/enumeration.rs
+++ starlark/src/values/types/enumeration.rs
@@ -170,7 +170,7 @@ impl<'v> EnumType<'v> {
         // We want to get the value of `me` into the function, but that doesn't work since it
         // might move between threads - so we create the NativeFunction and apply it later.
         NativeFunction::new(
-            move |_context, mut param_parser: ParametersParser| {
+            move |_eval, mut param_parser: ParametersParser| {
                 let typ_val = param_parser.next("me")?;
                 let val: Value = param_parser.next("value")?;
                 let typ = EnumType::from_value(typ_val).unwrap();
