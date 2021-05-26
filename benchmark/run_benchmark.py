4af80ad004dfbf2168f1659b4f6e56357d8edce4
Neil Mitchell <ndmitchell@fb.com>
2021-05-26 13:17:55 -0700

    Add a benchmarking script
    Summary: Want to have an easy way of running all the benchmarks robustly.
    
    Reviewed By: milend
    
    Differential Revision: D28707254
    
    fbshipit-source-id: ea69f9c958e0954f5c8d2e3b2a516cdab4c43995

---
+++ benchmark/run_benchmark.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+import tempfile
+import time
+from pathlib import Path
+
+
+def compile_starlark():
+    if "CARGO_TARGET_DIR" not in os.environ:
+        raise Exception("Must set CARGO_TARGET_DIR, so we can find Cargo outputs")
+    print("Building starlark")
+    subprocess.run("cargo build --release --bin=starlark", shell=True, check=True)
+    return os.environ["CARGO_TARGET_DIR"] + "/release/starlark"
+
+
+def generate_benchmarks(dir):
+    benchmark = Path(__file__).parent.joinpath("benchmark.py")
+
+    with open(benchmark, "r") as file:
+        src = file.read()
+
+    # Find all the benchmarks
+    benchmarks = [
+        x[4:-3]
+        for x in src.splitlines()
+        if x.startswith("def benchmark") and x.endswith("():")
+    ]
+
+    outputs = {}
+    for benchmark in benchmarks:
+        # Whichever one is committed, make sure we switch it for this one
+        src2 = src
+        for x in benchmarks:
+            src2 = src2.replace("print(" + x + "())", "print(" + benchmark + "())")
+        output = Path(dir).joinpath(benchmark + ".py")
+        with open(output, "w") as out:
+            out.write(src2)
+        outputs[benchmark] = output
+    return outputs
+
+
+def absh(a, b):
+    a_time = 0
+    b_time = 0
+    runs = 0
+
+    # Run a/b repeatedly, ignoring the first loop around
+    for i in range(6):
+        start_time = time.time()
+        subprocess.run(a, check=True, capture_output=True)
+        middle_time = time.time()
+        subprocess.run(b, check=True, capture_output=True)
+        end_time = time.time()
+
+        if i != 0:
+            a_time += middle_time - start_time
+            b_time += end_time - middle_time
+            runs += 1
+        print(".", end="", flush=True)
+
+    print("")
+    return (a_time / runs, b_time / runs)
+
+
+def main():
+    starlark = compile_starlark()
+    with tempfile.TemporaryDirectory() as dir:
+        benchmarks = generate_benchmarks(dir)
+        for name, file in benchmarks.items():
+            print("Benchmarking: " + name + " ", end="", flush=True)
+            (py, st) = absh(("python3", file), (starlark, file))
+            print("Python3 {:.2f}s, Starlark Rust {:.2f}s".format(py, st))
+
+
+if __name__ == "__main__":
+    main()
