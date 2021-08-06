8fc97bb69a107b098ec718267e6d939a005a896d
Neil Mitchell <ndmitchell@fb.com>
2021-08-06 09:48:39 -0700

    Optimise flame profiling
    Summary: When building a flame profile we don't disable GC (which keeps things faster), but that means we have to walk the entire historical call stack on each trace - which was taking the majority of the time. Solve that by caching each Value only once, and using an index. Provided the number of unique functions is low, this reduces the time significantly. In practice, reduced performing one flame graph from 70s to 30s (when 20s is the standard compilation time).
    
    Reviewed By: krallin
    
    Differential Revision: D30132958
    
    fbshipit-source-id: d0150ec772691018a9477edbde2d98bf2971169e

--- starlark/src/eval/runtime/flame_profile.rs
+++ starlark/src/eval/runtime/flame_profile.rs
@@ -18,6 +18,7 @@
 use crate as starlark;
 use crate::values::{Trace, Tracer, Value};
 use anyhow::Context;
+use gazebo::prelude::*;
 use std::{
     collections::{hash_map::Entry, HashMap},
     fs::File,
@@ -28,34 +29,63 @@ use std::{
     time::{Duration, Instant},
 };
 
-enum Frame<'v> {
-    Push(Value<'v>),
+/// Index into FlameData.values
+#[derive(Hash, PartialEq, Eq, Clone, Copy, Dupe)]
+struct ValueIndex(usize);
+
+/// Index into FlameData.map
+#[derive(Hash, PartialEq, Eq, Clone, Copy, Dupe)]
+struct ValuePtr(usize);
+
+impl ValueIndex {
+    fn lookup<T>(self, xs: &[T]) -> &T {
+        &xs[self.0]
+    }
+}
+
+impl ValuePtr {
+    fn new(x: Value) -> Self {
+        Self(x.ptr_value())
+    }
+}
+
+enum Frame {
+    Push(ValueIndex),
     Pop,
 }
 
 #[derive(Trace)]
-pub(crate) struct FlameProfile<'v> {
-    #[allow(clippy::box_vec)] // Deliberately box to reduce memory usage when disabled
-    frames: Option<Box<Vec<(Frame<'v>, Instant)>>>,
+pub(crate) struct FlameProfile<'v>(Option<Box<FlameData<'v>>>);
+
+/// In order to optimise GC (which otherwise quickly becomes O(n^2)) we have to
+/// dedupe the values, so store them in `values`, with a fast map to get them in `map`.
+/// Whenever we GC, regenerate map.
+#[derive(Default)]
+struct FlameData<'v> {
+    frames: Vec<(Frame, Instant)>,
+    values: Vec<Value<'v>>,
+    map: HashMap<ValuePtr, ValueIndex>,
 }
 
-unsafe impl<'v> Trace<'v> for Frame<'v> {
+unsafe impl<'v> Trace<'v> for FlameData<'v> {
     fn trace(&mut self, tracer: &Tracer<'v>) {
-        match self {
-            Frame::Push(x) => tracer.trace(x),
-            Frame::Pop => {}
+        self.values.trace(tracer);
+        // Have to rebuild the map, as its keyed by ValuePtr which changes on GC
+        self.map.clear();
+        for (i, x) in self.values.iter().enumerate() {
+            self.map.insert(ValuePtr::new(*x), ValueIndex(i));
         }
     }
 }
 
-struct Stacks {
-    name: String,
+struct Stacks<'a> {
+    name: &'a str,
     time: Duration,
-    children: HashMap<usize, Stacks>,
+    children: HashMap<ValueIndex, Stacks<'a>>,
 }
 
-impl Stacks {
-    fn blank(name: String) -> Self {
+impl<'a> Stacks<'a> {
+    fn blank(name: &'a str) -> Self {
         Stacks {
             name,
             time: Duration::default(),
@@ -63,22 +93,29 @@ impl Stacks {
         }
     }
 
-    fn new(frames: &[(Frame, Instant)]) -> Self {
-        let mut res = Stacks::blank("root".to_owned());
+    fn new(names: &'a [String], frames: &[(Frame, Instant)]) -> Self {
+        let mut res = Stacks::blank("root");
         let mut last_time = frames.first().map_or_else(Instant::now, |x| x.1);
-        res.add(&mut frames.iter(), &mut last_time);
+        res.add(names, &mut frames.iter(), &mut last_time);
         res
     }
 
-    fn add(&mut self, frames: &mut slice::Iter<(Frame, Instant)>, last_time: &mut Instant) {
+    fn add(
+        &mut self,
+        names: &'a [String],
+        frames: &mut slice::Iter<(Frame, Instant)>,
+        last_time: &mut Instant,
+    ) {
         while let Some((frame, time)) = frames.next() {
             self.time += time.duration_since(*last_time);
             *last_time = *time;
             match frame {
                 Frame::Pop => return,
-                Frame::Push(v) => match self.children.entry(v.ptr_value()) {
-                    Entry::Occupied(mut e) => e.get_mut().add(frames, last_time),
-                    Entry::Vacant(e) => e.insert(Stacks::blank(v.to_repr())).add(frames, last_time),
+                Frame::Push(i) => match self.children.entry(*i) {
+                    Entry::Occupied(mut e) => e.get_mut().add(names, frames, last_time),
+                    Entry::Vacant(e) => e
+                        .insert(Stacks::blank(i.lookup(names).as_str()))
+                        .add(names, frames, last_time),
                 },
             }
         }
@@ -90,7 +127,7 @@ impl Stacks {
         if !buffer.is_empty() {
             buffer.push(';')
         }
-        buffer.push_str(&self.name);
+        buffer.push_str(self.name);
         let count = self.time.as_millis();
         if count > 0 {
             writeln!(file, "{} {}", buffer, count)?;
@@ -110,37 +147,46 @@ impl Stacks {
 
 impl<'v> FlameProfile<'v> {
     pub(crate) fn new() -> Self {
-        Self { frames: None }
+        Self(None)
     }
 
     pub(crate) fn enable(&mut self) {
-        self.frames = Some(box Vec::new());
+        self.0 = Some(box FlameData::default());
     }
 
     pub fn record_call_enter(&mut self, function: Value<'v>) {
-        if let Some(box x) = &mut self.frames {
-            x.push((Frame::Push(function), Instant::now()))
+        if let Some(box x) = &mut self.0 {
+            let ind = match x.map.entry(ValuePtr::new(function)) {
+                Entry::Occupied(e) => *e.get(),
+                Entry::Vacant(e) => {
+                    let res = ValueIndex(x.values.len());
+                    x.values.push(function);
+                    e.insert(res);
+                    res
+                }
+            };
+            x.frames.push((Frame::Push(ind), Instant::now()))
         }
     }
 
     pub fn record_call_exit(&mut self) {
-        if let Some(box x) = &mut self.frames {
-            x.push((Frame::Pop, Instant::now()))
+        if let Some(box x) = &mut self.0 {
+            x.frames.push((Frame::Pop, Instant::now()))
         }
     }
 
     // We could expose profile on the Heap, but it's an implementation detail that it works here.
     pub(crate) fn write(&self, filename: &Path) -> Option<anyhow::Result<()>> {
-        self.frames
+        self.0
             .as_ref()
             .map(|box x| Self::write_enabled(x, filename))
     }
 
-    fn write_enabled(frames: &[(Frame<'v>, Instant)], filename: &Path) -> anyhow::Result<()> {
+    fn write_enabled(x: &FlameData, filename: &Path) -> anyhow::Result<()> {
         let file = File::create(filename).with_context(|| {
             format!("When creating profile output file `{}`", filename.display())
         })?;
-        Self::write_profile_to(frames, file).with_context(|| {
+        Self::write_profile_to(x, file).with_context(|| {
             format!(
                 "When writing to profile output file `{}`",
                 filename.display()
@@ -148,10 +194,11 @@ impl<'v> FlameProfile<'v> {
         })
     }
 
-    fn write_profile_to(frames: &[(Frame<'v>, Instant)], file: impl Write) -> io::Result<()> {
+    fn write_profile_to(x: &FlameData, file: impl Write) -> io::Result<()> {
         // Need to write out lines which look like:
         // root;calls1;calls2 1
         // All the numbers at the end must be whole numbers (we use milliseconds)
-        Stacks::new(frames).render(file)
+        let names = x.values.map(|x| x.to_repr());
+        Stacks::new(&names, &x.frames).render(file)
     }
 }
