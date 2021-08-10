1346c209006df6cc9b3c54a12bc62df0870bd5b6
Stiopa Koltsov <nga@fb.com>
2021-08-09 21:02:05 -0700

    Create MapHolder::Empty in MapHolder::with_capacity
    Summary: Empty map should be represented by `MapHolder::Empty`.
    
    Reviewed By: cjhopman
    
    Differential Revision: D30211525
    
    fbshipit-source-id: e9e59343a0f49778407e8925630022a9085f1c68

--- starlark/src/collections/small_map.rs
+++ starlark/src/collections/small_map.rs
@@ -308,7 +308,9 @@ impl<K, V> ExactSizeIterator for MHIntoIter<K, V> {
 
 impl<K, V> MapHolder<K, V> {
     fn with_capacity(n: usize) -> Self {
-        if n <= THRESHOLD {
+        if n == 0 {
+            MapHolder::Empty
+        } else if n <= THRESHOLD {
             MapHolder::Vec(VecMap::with_capacity(n))
         } else {
             MapHolder::Map(IndexMap::with_capacity_and_hasher(n, Default::default()))
