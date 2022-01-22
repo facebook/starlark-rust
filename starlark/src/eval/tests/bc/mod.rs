80d572c18921fe18882570bb090d11450f479ca7
Stiopa Koltsov <nga@fb.com>
2022-01-22 12:46:56 -0800

    tests::bc::expr
    Summary: Split `mod bc`, to add more tests.
    
    Reviewed By: ndmitchell
    
    Differential Revision: D33718047
    
    fbshipit-source-id: 56761acac31cc26e22174294acd9d280069d29b5

--- starlark/src/eval/tests/bc/mod.rs
+++ starlark/src/eval/tests/bc/mod.rs
@@ -17,8 +17,9 @@
 
 //! Bytecode generation tests.
 
+mod expr;
+
 use crate::{
-    assert,
     assert::Assert,
     eval::{bc::opcode::BcOpcode, FrozenDef},
 };
@@ -34,66 +35,4 @@ pub(crate) fn test_instrs(expected: &[BcOpcode], def_program: &str) {
     let mut opcodes = def.bc().instrs.opcodes();
     assert_eq!(Some(BcOpcode::End), opcodes.pop());
     assert_eq!(expected, opcodes);
-}
-
-#[test]
-fn test_type() {
-    test_instrs(
-        &[BcOpcode::LoadLocal, BcOpcode::Type, BcOpcode::Return],
-        "def test(x): return type(x)",
-    );
-}
-
-#[test]
-fn test_percent_s_one() {
-    test_instrs(
-        &[BcOpcode::LoadLocal, BcOpcode::PercentSOne, BcOpcode::Return],
-        "def test(x): return '((%s))' % x",
-    )
-}
-
-#[test]
-fn test_format_one() {
-    test_instrs(
-        &[BcOpcode::LoadLocal, BcOpcode::FormatOne, BcOpcode::Return],
-        "def test(x): return '(({}))'.format(x)",
-    )
-}
-
-#[test]
-fn test_percent_s_one_format_one_eval() {
-    assert::pass(
-        r#"
-load("assert.star", "assert")
-
-def test(x):
-    return ("<{}>".format(x), "<%s>" % x)
-
-assert.eq(("<1>", "<1>"), test(1))
-# Test format does not accidentally call `PercentSOne`.
-assert.eq(("<(1,)>", "<1>"), test((1,)))
-"#,
-    );
-}
-
-#[test]
-fn test_spec_exec_list() {
-    // `list` function is const-evaluated and the resulting list is compiled as list instruction.
-    test_instrs(
-        &[BcOpcode::ListOfConsts, BcOpcode::Return],
-        "def test(): return list((10, 20))",
-    )
-}
-
-#[test]
-fn test_call_maybe_known_method() {
-    test_instrs(
-        &[
-            BcOpcode::LoadLocalAndConst,
-            BcOpcode::CallMaybeKnownMethodPos,
-            BcOpcode::Pop,
-            BcOpcode::ReturnNone,
-        ],
-        "def test(x): x.append(1)",
-    );
 }
