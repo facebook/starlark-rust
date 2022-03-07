f1113154d76fde948bdaef040983c3331c69b8f9
Bob Yang <bobyf@fb.com>
2022-03-07 12:16:41 -0800

    Mark StringIterGen as not serializable
    Reviewed By: ndmitchell
    
    Differential Revision: D34663892
    
    fbshipit-source-id: 8897b54f25dfa75177938f8b95ece3faeca5e859

--- starlark/src/values/types/string/iter.rs
+++ starlark/src/values/types/string/iter.rs
@@ -24,7 +24,7 @@ use crate as starlark;
 use crate::values::{Heap, StarlarkValue, Value, ValueLike};
 
 /// An opaque iterator over a string, produced by elems/codepoints
-#[derive(Debug, Trace, Coerce, Display, Freeze)]
+#[derive(Debug, Trace, Coerce, Display, Freeze, NoSerialize)]
 #[display(fmt = "iterator")]
 #[repr(C)]
 struct StringIteratorGen<V> {
