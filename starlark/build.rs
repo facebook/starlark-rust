c0ee0da7eb1ef84c5171cedfa7c7fdee28580ebd
Stiopa Koltsov <nga@meta.com>
2022-11-06 12:44:36 -0800

    --cfg=rustc_nightly in Cargo builds
    Reviewed By: ndmitchell
    
    Differential Revision: D41061862
    
    fbshipit-source-id: 53006710077fee535c41e3c958ec8a73f385dc78

--- starlark/build.rs
+++ starlark/build.rs
@@ -17,6 +17,7 @@
 
 fn main() {
     lalrpop();
+    rust_nightly();
 }
 
 fn lalrpop() {
@@ -27,4 +28,26 @@ fn lalrpop() {
         .emit_report(true)
         .process_file(source)
         .unwrap();
+}
+
+fn rust_nightly() {
+    let rustc = std::env::var("RUSTC").unwrap();
+    let version = std::process::Command::new(rustc)
+        .arg("--version")
+        .output()
+        .unwrap();
+
+    assert!(version.status.success());
+
+    // Nightly output:
+    // rustc 1.64.0-nightly (affe0d3a0 2022-08-05)
+    // Stable output:
+    // rustc 1.64.0 (a55dd71d5 2022-09-19)
+
+    let stdout = String::from_utf8(version.stdout).unwrap();
+    assert!(stdout.contains("rustc"), "Sanity check");
+    let nightly = stdout.contains("nightly");
+    if nightly {
+        println!("cargo:rustc-cfg=rust_nightly");
+    }
 }
