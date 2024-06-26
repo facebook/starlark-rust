4339b74c32d653ff51ab9803138fe9baa60461b1
Grzegorz Lukasik <Grzegorz.Lukasik@aptiv.com>
2024-06-26 10:19:08 +0200

    Fix unwrap panic in bazel-lsp for Label

--- starlark/src/docs/markdown.rs
+++ starlark/src/docs/markdown.rs
@@ -136,7 +136,7 @@ fn render_function_parameters(params: &[DocParam]) -> Option<String> {
             let docs = render_doc_string(DSOpts::Combined, docs).unwrap_or_default();
 
             let mut lines_iter = docs.lines();
-            let first_line = lines_iter.next().unwrap();
+            let first_line = lines_iter.next().unwrap_or_default();
             let rest_of_lines: Vec<&str> = lines_iter.collect();
 
             let _ = writeln!(output, "* `{name}`: {first_line}");
